dee8e5c568330316c3491886d0656e0e91e8e806
betty2310 <huynh.dh2310@gmail.com>
2023-02-04 11:58:56 +0700

    feat(): init timeline

--- src/main/java/com/hust/historical/TimeLine.java
+++ src/main/java/com/hust/historical/TimeLine.java
@@ -1,4 +1,24 @@
 package com.hust.historical;
 
+import com.hust.historical.model.Dynasty;
+
+import java.util.ArrayList;
+
 public class TimeLine {
+    private ArrayList<Dynasty> dynasties;
+    public TimeLine() {
+        dynasties = new ArrayList<Dynasty>();
+    }
+    public boolean addDynasty(Dynasty dynasty) {
+        if(dynasties.contains(dynasty))
+            return false;
+        dynasties.add(dynasty);
+        return true;
+    }
+    public boolean removeDynasty(Dynasty dynasty) {
+        if(!dynasties.contains(dynasty))
+            return false;
+        dynasties.remove(dynasty);
+        return true;
+    }
 }
